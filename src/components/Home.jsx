72cefa0792d5c08aa9def6fc017ae6fe6a096a96
Abhinesh612 <abhineshsharma612@gmail.com>
2022-06-26 18:42:26 +0530

    16-new-commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import Bottumwave from './Assests/waveBottum.svg';
 const Home = () => {
 	return (
 		<div id='home-spot' name='home' className='bg-center bg-no-repeat h-screen w-full bg-[#5CDB95] bg-cover' style={{ backgroundImage: `url(${Background})`}}>
-			<div className='w-full h-full flex justify-center'>
+			<div className='w-full h-full'>
 				<div className='text-[#EDF5E1] font-Zcool ml-[50px] md:ml-[150px] h-full mx-auto flex flex-col justify-center'>
 					<h1 className='text-5xl md:text-7xl lg:text-9xl font-bold'>ABHINESH</h1>
 					<h2 className='text-2xl md:text-3xl lg:text-4xl'>HIMALAYAN RANGER</h2>
